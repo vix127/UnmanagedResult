86834460aceb859b239990fbfb6fcfd7859b74c6
Viktor Stojanovic <viktor2006stojanovic@gmail.com>
2026-08-03 21:00:30 +0200

    Add Value & Error properties Add type constrains for interface

--- src/IResult.cs
+++ src/IResult.cs
@@ -1,6 +1,9 @@
 ﻿namespace UnmanagedResult;
 
-public interface IResult<TResult, TError>
+public interface IResult<out TValue, TError>
+    where TValue : unmanaged
+    where TError : unmanaged, IError
 {
-
+    public TValue Value { get; }
+    public TError Error { get; }
 }
