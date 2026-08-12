8416c5fe0c519d41aecb39ad3e1ce9d0f54f1175
Viktor Stojanovic <viktor2006stojanovic@gmail.com>
2026-08-12 17:17:08 +0200

    Add concrete result implementation

---
+++ src/Result.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnmanagedResult;
+
+public struct Result<TValue, TEnum> : IResult<TValue, Error<TEnum>>
+    where TValue : unmanaged
+    where TEnum : unmanaged, Enum
+{
+    public TValue Value => throw new System.NotImplementedException();
+
+    public Error<TEnum> Error => default;
+}
