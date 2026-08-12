4ee78181f005d183975bcde0286b959d4272eb57
Viktor Stojanovic <viktor2006stojanovic@gmail.com>
2026-08-12 18:14:13 +0200

    Add ICodeError interface

---
+++ src/ICodeError.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) 2026 Viktor Stojanović. All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+using System;
+
+namespace UnmanagedResult;
+
+public interface ICodeError<TEnum> : IError
+    where TEnum : unmanaged, Enum
+{
+    public TEnum Code { get; }
+}
